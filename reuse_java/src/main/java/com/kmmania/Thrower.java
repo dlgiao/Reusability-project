9818a54b2a69cf73402e956f9bed27fc5fffb65d
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-26 15:06:41 +0200

    Implement Thrower

---
+++ reuse_java/src/main/java/com/kmmania/Thrower.java
@@ -0,0 +1,34 @@
+package com.kmmania;
+
+/**
+ * The Thrower class implements the {@link Athlete} class by composition and represents a thrower athlete.
+ *
+ * @author Dac Linh Giao NGUYEN
+ * @version 0.1
+ * @see Athlete
+ */
+public class Thrower {
+    private final String firstName;
+    private final String lastName;
+    private final Athlete athlete;
+
+    /**
+     * Constructs a Thrower object with the specified first name, last name, and specialty.
+     *
+     * @param firstName  the first name of the thrower
+     * @param lastName   the last name of the thrower
+     * @param speciality the specialty of the thrower
+     */
+    public Thrower(String firstName, String lastName, String speciality) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.athlete = new Athlete(speciality);
+    }
+
+    /**
+     * Displays the thrower's information including name and specialty.
+     */
+    public void display() {
+        System.out.printf("My name is %s %s. %s\n", firstName, lastName, athlete.mySpeciality());
+    }
+}
