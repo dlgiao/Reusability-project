b71a9c81ede958f4eb611010795312a54825dcb2
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-26 17:10:29 +0200

    Update

--- reuse_java/src/main/java/com/kmmania/Main.java
+++ reuse_java/src/main/java/com/kmmania/Main.java
@@ -1,7 +1,29 @@
 package com.kmmania;
 
+/**
+ * The Main class demonstrates the usage of various athlete classes.
+ *
+ * @author Dac Linh Giao NGUYEN
+ * @version 0.1
+ */
 public class Main {
+
+    /**
+     * The main method instantiates objects of different athlete types and displays their information.
+     *
+     * @param args the command-line arguments (not used)
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Sprinter usain = new Sprinter("Usain", "Bolt", "sprinting");
+        usain.display();
+
+        Thrower ryan = new Thrower("Ryan", "Crouser", "shot put");
+        ryan.display();
+
+        DistanceRunner eliud = new DistanceRunner("Eliud", "Kipchoge", "marathoner");
+        eliud.display();
+
+        Jumper marco = new Jumper("Marco", "Tamberi", "high jump");
+        marco.display();
     }
 }
