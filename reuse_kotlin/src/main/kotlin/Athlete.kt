79270be56be0da39f18f12cba09057f659b4f2c2
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-26 18:02:32 +0200

    Set Athlete open

--- reuse_kotlin/src/main/kotlin/Athlete.kt
+++ reuse_kotlin/src/main/kotlin/Athlete.kt
@@ -1,5 +1,5 @@
 package com.kmmania
 
-class Athlete(private val speciality: String) {
+open class Athlete(private val speciality: String) {
     fun mySpeciality(): String = "My speciality is $speciality."
 }
