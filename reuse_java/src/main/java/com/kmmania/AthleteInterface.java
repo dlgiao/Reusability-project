b7d3838ffa6f4922c96a8b0694cb3472febfcb7f
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-26 15:37:15 +0200

    Implement AthleteInterface

---
+++ reuse_java/src/main/java/com/kmmania/AthleteInterface.java
@@ -0,0 +1,16 @@
+package com.kmmania;
+
+/**
+ * The AthleteInterface interface defines a method for retrieving an athlete's specialty.
+ *
+ * @author Dac Linh Giao NGUYEN
+ * @version 0.1
+ */
+public interface AthleteInterface {
+    /**
+     * Returns a string representing the athlete's specialty.
+     *
+     * @return a string representing the athlete's specialty
+     */
+    String mySpeciality();
+}
