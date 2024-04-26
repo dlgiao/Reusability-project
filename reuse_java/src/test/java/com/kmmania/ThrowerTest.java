3bcf5c77ce0bb1f98c534edf741c5087009434fa
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-26 15:12:24 +0200

    Implement Thrower unit tests

---
+++ reuse_java/src/test/java/com/kmmania/ThrowerTest.java
@@ -0,0 +1,29 @@
+package com.kmmania;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+public class ThrowerTest {
+
+    @Test
+    void display_DisplaysCorrectInformation() {
+        // Given
+        String firstName = "Alice";
+        String lastName = "Smith";
+        String speciality = "Throwing";
+        Athlete mockAthlete = mock(Athlete.class);
+        when(mockAthlete.mySpeciality()).thenReturn("My speciality is " + speciality + ".");
+        Thrower thrower = new Thrower(firstName, lastName, speciality);
+        thrower.setAthlete(mockAthlete);
+
+        // When
+        thrower.display();
+
+        // Then
+        assertThat(thrower.getFirstName()).isEqualTo(firstName);
+        assertThat(thrower.getLastName()).isEqualTo(lastName);
+        verify(mockAthlete).mySpeciality();
+    }
+}
