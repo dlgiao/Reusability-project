4c2a6dc1d32a3e7b8ee8983753a7115ae2528a1a
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-26 18:19:20 +0200

    Implement ryan

--- reuse_kotlin/src/main/kotlin/Main.kt
+++ reuse_kotlin/src/main/kotlin/Main.kt
@@ -3,4 +3,7 @@ package com.kmmania
 fun main() {
     val usain = Sprinter("Usain", "Bolt", "sprinting")
     usain.display()
+
+    val ryan = Thrower("Ryan", "Crouser", "shot put")
+    ryan.display()
 }
