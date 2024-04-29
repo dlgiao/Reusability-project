660be959d4e36da2f0d4fa72871829a1fbdc56f7
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-29 11:12:21 +0200

    Implement comments

--- reuse_go/athlete/Athlete.go
+++ reuse_go/athlete/Athlete.go
@@ -1,21 +1,26 @@
+// Package athlete provides the struct reusability by composition.
 package athlete
 
 import "fmt"
 
+// Athlete is a base struct representing an athlete.
 type Athlete struct {
 	Speciality string
 }
 
+// MySpeciality retrieves the speciality of the athlete.
 func (a *Athlete) MySpeciality() string {
 	return "My speciality is " + a.Speciality + "."
 }
 
+// Sprinter is a struct representing a Sprinter, utilizing composition with the Athlete class.
 type Sprinter struct {
 	Firstname string
 	Lastname  string
 	Athlete   Athlete
 }
 
+// Display displays the information of the Sprinter including their full name and speciality.
 func (s *Sprinter) Display() {
 	fmt.Printf("My name is %s %s. %v\n",
 		s.Firstname,
@@ -24,12 +29,14 @@ func (s *Sprinter) Display() {
 	)
 }
 
+// Thrower is a struct representing a Thrower, utilizing composition with the Athlete class.
 type Thrower struct {
 	Firstname string
 	Lastname  string
 	Athlete   Athlete
 }
 
+// Display displays the information of the Thrower including their full name and speciality.
 func (t *Thrower) Display() {
 	fmt.Printf("My name is %s %s. %v\n",
 		t.Firstname,
