12096e9b7fe9aa19af30c6e64560131ab0a68558
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-04-29 10:39:33 +0200

    Implement Sprinter struct

--- reuse_go/Athlete.go
+++ reuse_go/Athlete.go
@@ -1,9 +1,25 @@
 package reuse_go
 
+import "fmt"
+
 type Athlete struct {
 	speciality string
 }
 
 func (a *Athlete) MySpeciality() string {
 	return "My speciality is " + a.speciality + "."
+}
+
+type Sprinter struct {
+	firstName string
+	lastName  string
+	athlete   Athlete
+}
+
+func (s *Sprinter) Display() {
+	fmt.Printf("My name is %s %s. %v\n",
+		s.firstName,
+		s.lastName,
+		s.athlete.MySpeciality(),
+	)
 }
