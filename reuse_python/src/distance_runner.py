ab55cada333a77715f7fca15eb2332dddd911daa
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 12:04:44 +0200

    Implement DistanceRunner

---
+++ reuse_python/src/distance_runner.py
@@ -0,0 +1,52 @@
+"""
+Athlete Reusability Project
+
+This project demonstrates different ways to reuse classes in Python:
+- Inheritance
+- Composition
+- Composition through interface implementation
+
+Classes:
+    Athlete: Represents an athlete with a specific specialty.
+    Sprinter: Represents a sprinter, inheriting from Athlete.
+    Thrower: Represents a thrower, utilizing Athlete class through composition.
+    AthleteInterface: Interface defining the method for athlete specialty.
+    AthleteImpl: Class implementing the AthleteInterface.
+    DistanceRunner: Represents a distance runner, using Athlete interface through composition.
+    Jumper: Represents a jumper, using Athlete interface through composition.
+
+Usage:
+    Import the required classes and create instances of different types of athletes.
+    Each athlete instance can display information about themselves using the `display()` method.
+
+
+Author: Dac Linh Giao NGUYEN
+Date: 2024
+
+"""
+
+
+class DistanceRunner:
+    """Class representing a distance runner."""
+
+    def __init__(self, first_name, last_name, athlete):
+        """
+        Initializes a DistanceRunner object with a given first name, last name, and athlete.
+
+        Args:
+            first_name (str): The first name of the distance runner.
+            last_name (str): The last name of the distance runner.
+            athlete (AthleteBase): An Athlete object representing the distance runner's specialty.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.athlete = athlete
+
+    def display(self):
+        """
+        Displays information about the distance runner.
+
+        Returns:
+            None
+        """
+        print(f"My name is {self.first_name} {self.last_name}. {self.athlete.my_speciality()}")
