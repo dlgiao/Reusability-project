0b5d701c6e4049b6867a64827d7461cc1cef9ffa
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 16:46:47 +0200

    Implement Thrower

---
+++ reuse_js/src/thrower.js
@@ -0,0 +1,34 @@
+/**
+ * Represents a thrower athlete using composition.
+ *
+ * @author Dac Linh Giao NGUYEN
+ * @version 0.1
+ * @since 0.1
+ */
+class Thrower {
+    /**
+     * Create a thrower athlete.
+     *
+     * @param {string} firstName - The first name of the thrower.
+     * @param {string} lastName - The last name of the thrower.
+     * @param {Athlete} athlete - The speciality of the thrower.
+     */
+    constructor(
+        firstName,
+        lastName,
+        athlete
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.athlete = athlete;
+    }
+
+    /**
+     * Display thrower's information including name and speciality.
+     */
+    display() {
+        console.log(`My name is ${this.firstName} ${this.lastName}. ${this.athlete.mySpeciality()}`);
+    }
+}
+
+module.exports.Thrower = Thrower;
