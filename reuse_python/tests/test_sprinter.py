282469a518abda5a50dde4bb4230174988dcd58d
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 11:31:03 +0200

    Reformat file

--- reuse_python/tests/test_sprinter.py
+++ reuse_python/tests/test_sprinter.py
@@ -2,10 +2,12 @@ from src.sprinter import Sprinter
 
 
 class TestSprinter:
-    def setup_method(self):
+    def setup_method(self, method):
+        print(f"Setting up {method}")
         self.sprinter = Sprinter("Usain", "Bolt", "sprinting")
 
-    def teardown_method(self):
+    def teardown_method(self, method):
+        print(f"Tearing down {method}")
         del self.sprinter
 
     def test_display_method(self, capsys):
