f149eb59db9b5146c71aa7059860a89d2ae9b990
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 16:06:35 +0200

    Implement Sprinter

---
+++ reuse_js/src/sprinter.js
@@ -0,0 +1,33 @@
+import Athlete from "./athlete";
+
+/**
+ * Represents a sprinter.
+ *
+ * @extends Athlete
+ * @author Dac Linh Giao NGUYEN
+ * @version 0.1
+ * @since 0.1
+ */
+class Sprinter extends Athlete {
+    /**
+     * Create a sprinter.
+     *
+     * @param {string} firstName - The first name of the sprinter.
+     * @param {string} lastName - The last name of the sprinter.
+     * @param {string} speciality - The speciality of the sprinter.
+     */
+    constructor(firstName, lastName, speciality) {
+        super(speciality);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * Display sprinter's information including name and speciality.
+     */
+    display() {
+        console.log(`My name is ${this.firstName} ${this.lastName}. ${super.mySpeciality()}`);
+    }
+}
+
+module.exports = Sprinter;
