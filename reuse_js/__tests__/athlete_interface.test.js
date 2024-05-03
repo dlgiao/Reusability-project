ed867e8428ae27484037faf8d25fd523cddb166f
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 17:58:15 +0200

    Implement AthleteInterface unit tests

---
+++ reuse_js/__tests__/athlete_interface.test.js
@@ -0,0 +1,21 @@
+const AthleteInterface = require("../src/athlete_interface");
+
+describe("AthleteInterface", () => {
+    describe("mySpeciality", () => {
+        it("should be implemented by subclasses", () => {
+            // mock for mySpeciality() method
+            const mySpecialityMock = jest.fn(() => "running");
+
+            // Athlete mock with mySpeciality() method
+            const athleteMock = {
+                mySpeciality: mySpecialityMock
+            };
+
+            // mySpeciality of Athlete mock method call
+            const result = athleteMock.mySpeciality();
+
+            expect(result).toBe("running");
+            expect(mySpecialityMock).toHaveBeenCalledTimes(1);
+        });
+    });
+});
