cd414e40d5659c82c5960e0591e9bab28e09e8aa
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 17:41:16 +0200

    Implement AthleteInterface

---
+++ reuse_js/src/athlete_interface.js
@@ -0,0 +1,20 @@
+/**
+ * Represents an interface for Athlete.
+ *
+ * @interface
+ * @author Dac Linh Giao NGUYEN
+ * @version 0.1
+ * @since 0.1
+ */
+class AthleteInterface {
+    /**
+     * Method to get the speciality of the athlete.
+     *
+     * @abstract
+     * @returns {string} The speciality of the athlete.
+     */
+    mySpeciality() {
+    }
+}
+
+module.exports = AthleteInterface;
