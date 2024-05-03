039432ff98d38beb344f0f3a0d5a44a946d7e344
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 16:04:53 +0200

    Implement Athlete unit tests

---
+++ reuse_js/__tests__/athlete.test.js
@@ -0,0 +1,15 @@
+const Athlete = require('../src/athlete');
+
+describe('Athlete', () => {
+    // Test constructor
+    test("constructor should set the speciality correctly", () => {
+        const athlete = new Athlete("running");
+        expect(athlete.speciality).toBe("running");
+    });
+
+    // Test mySpeciality method
+    test("mySpeciality should return correct string", () => {
+        const athlete = new Athlete("running");
+        expect(athlete.mySpeciality()).toBe("My speciality is running.");
+    });
+})
