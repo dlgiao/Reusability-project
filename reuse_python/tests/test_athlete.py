857caae40fdb30e1980aece0a594a5b4f9438158
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 11:31:43 +0200

    Reformat file

--- reuse_python/tests/test_athlete.py
+++ reuse_python/tests/test_athlete.py
@@ -4,9 +4,11 @@ from src.athlete import Athlete
 class TestAthlete:
 
     def setup_method(self, method):
+        print(f"Setting up {method}")
         self.athlete = Athlete("running")
 
     def teardown_method(self, method):
+        print(f"Tearing down {method}")
         del self.athlete
 
     def test_my_speciality_method(self):
