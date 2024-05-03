ad2c96b12c415f2c882529ff86bc27f5dd221bdf
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2024-05-03 11:35:19 +0200

    Implement athlete_interface.py

---
+++ reuse_python/src/athlete_interface.py
@@ -0,0 +1,36 @@
+"""
+Athlete Reusability Project
+
+This project demonstrates different ways to reuse classes in Python:
+- Inheritance
+- Composition
+- Composition through interface implementation
+
+Classes:
+    Athlete: Represents an athlete with a specific specialty.
+    Sprinter: Represents a sprinter, inheriting from Athlete.
+    Thrower: Represents a thrower, utilizing Athlete class through composition.
+    AthleteInterface: Interface defining the method for athlete specialty.
+    AthleteImpl: Class implementing the AthleteInterface.
+    DistanceRunner: Represents a distance runner, using Athlete interface through composition.
+    Jumper: Represents a jumper, using Athlete interface through composition.
+
+Usage:
+    Import the required classes and create instances of different types of athletes.
+    Each athlete instance can display information about themselves using the `display()` method.
+
+
+Author: Dac Linh Giao NGUYEN
+Date: 2024
+
+"""
+from abc import abstractmethod
+
+
+class AthleteInterface:
+    """Interface defining the method for athlete specialty."""
+
+    @abstractmethod
+    def my_speciality(self):
+        """Abstract method to be implemented by subclasses."""
+        pass
